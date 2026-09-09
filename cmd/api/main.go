385adc2677221ed7de3c083068d1b3a5c9522290
endisuwandi <endisuwandi64@gmail.com>
2026-09-09 13:40:34 +0700

    chore: initialize router code used net/htpp to main.go file

---
+++ cmd/api/main.go
@@ -0,0 +1,34 @@
+package main
+
+
+import (
+
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+
+
+func main (){
+	mux := newServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWritter, r *http.Request){
+		w.Header().Set("Content-Type", "aplication/json")
+		w.WriteHeader(http.statusOK)
+		json.NewEncoder(w).Encode(map[string]string{
+			"status": "healty",
+			"service": "clothify-go-api"
+		})
+
+
+	})
+
+
+}
